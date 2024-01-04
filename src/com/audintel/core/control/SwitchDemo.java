3484c26d79835a812822181c28f08724d4435f05
suresh.kavili <suresh.kavili@gmail.com>
2024-01-04 18:35:29 +0530

    Corejava

---
+++ src/com/audintel/core/control/SwitchDemo.java
@@ -0,0 +1,86 @@
+package com.audintel.core.control;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Stack;
+import java.util.Vector;
+
+public class SwitchDemo {
+    public static void main(String[] args) {
+        //m1();
+        //m2();
+        //m3();
+        Vector <Integer> v = new Vector<>();
+        v.add(11);
+        v.add(21);
+        m4(v);
+    }
+
+    private static void m4(Collection c) {
+
+    switch(c) {
+
+        case Stack s -> s.get(1);
+        case Vector v -> v.get(1);
+        case ArrayList a -> a.get(1);
+        default -> System.out.println();
+
+
+
+    }
+}
+    private static void m3() {
+
+
+        switch(3) {
+
+            case 1:
+                System.out.println("One");
+                return ;
+            case 2:
+                System.out.println("Two");
+                return ;
+            case 3:
+                System.out.println("Three");
+                return ;
+            default:
+                System.out.println("Other value");
+                return ;
+        }
+    }
+
+    private static int m2() {
+
+            switch(3) {
+
+            case 1:
+                System.out.println("One");
+                return 1;
+            case 2:
+                System.out.println("Two");
+                return 2;
+            case 3:
+                System.out.println("Three");
+                return 3;
+            default:
+                System.out.println("Other value");
+                return 9999;
+        }
+    }
+
+    private static void m1() {
+        switch(3) {
+
+            case 1:
+                System.out.println("One");
+            case 2:
+                System.out.println("Two");
+            case 3:
+                System.out.println("Three");
+                break;
+            default:
+                System.out.println("Other value");
+        }
+    }
+
+}
