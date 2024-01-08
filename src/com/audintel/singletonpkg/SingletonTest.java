3576570f6fd230f3fab99b42df7dc20a3f604fb8
suresh.kavili <suresh.kavili@gmail.com>
2024-01-08 16:47:23 +0530

    Corejava

---
+++ src/com/audintel/singletonpkg/SingletonTest.java
@@ -0,0 +1,21 @@
+package com.audintel.singletonpkg;
+
+public class SingletonTest {
+    public static void main(String[] args) {
+//        SingletonClass s = new SingletonClass();
+        SingletonClass s1 = SingletonClass.getInstance();
+
+    }
+}
+
+class SingletonClass{
+    private static SingletonClass s = null;
+    private SingletonClass(){
+        System.out.println("Object Initialised");
+    }
+
+    public static synchronized SingletonClass getInstance(){
+        if(s==null) s = new SingletonClass();
+        return s;
+    }
+}
