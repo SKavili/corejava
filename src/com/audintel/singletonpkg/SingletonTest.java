ffe00d72dc91652989ef5685d66eb0197f31b169
audintelsaim <155418113+audintelsaim@users.noreply.github.com>
2024-01-08 15:45:02 +0530

    added synchronized

--- src/com/audintel/singletonpkg/SingletonTest.java
+++ src/com/audintel/singletonpkg/SingletonTest.java
@@ -3,19 +3,19 @@ package com.audintel.singletonpkg;
 public class SingletonTest {
     public static void main(String[] args) {
 //        SingletonClass s = new SingletonClass();
-        SingletonClass s1 = SingletonClass.getInstatnce();
+        SingletonClass s1 = SingletonClass.getInstance();
 
     }
 }
 
 class SingletonClass{
-    private static SingletonClass s;
+    private static SingletonClass s = null;
     private SingletonClass(){
         System.out.println("Object Initialised");
     }
 
-    public static SingletonClass getInstatnce(){
-        s = new SingletonClass();
+    public static synchronized SingletonClass getInstance(){
+        if(s==null) s = new SingletonClass();
         return s;
     }
 }
