d2ae6d37f8c703ccaad42755dfa14e468167b2a4
audintelsaim <155418113+audintelsaim@users.noreply.github.com>
2024-01-08 15:24:22 +0530

    worked on Static

---
+++ src/com/audintel/singletonpkg/SingletonTest.java
@@ -0,0 +1,21 @@
+package com.audintel.singletonpkg;
+
+public class SingletonTest {
+    public static void main(String[] args) {
+//        SingletonClass s = new SingletonClass();
+        SingletonClass s1 = SingletonClass.getInstatnce();
+
+    }
+}
+
+class SingletonClass{
+    private static SingletonClass s;
+    private SingletonClass(){
+        System.out.println("Object Initialised");
+    };
+
+    public static SingletonClass getInstatnce(){
+        s = new SingletonClass();
+        return s;
+    }
+}
