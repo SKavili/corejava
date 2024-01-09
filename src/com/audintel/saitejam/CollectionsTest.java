8bd1833f24b937501623b2216b1b912332864dcb
audintelsaim <155418113+audintelsaim@users.noreply.github.com>
2024-01-09 16:08:39 +0530

    Implemented Collections functionalities

--- src/com/audintel/saitejam/CollectionsTest.java
+++ src/com/audintel/saitejam/CollectionsTest.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 
-public class CollectionsTest {
+public class CollectionsTest{
     public static void main(String[] args) {
         ArrayList<Integer> ar = new ArrayList<Integer>(Arrays.asList(4,8,45,34,576,7,45344,235456,56,8));
         ArrayList<Integer> arCopy = (ArrayList) ar.clone();
@@ -26,8 +26,6 @@ public class CollectionsTest {
         ArrayList<Integer> ar2 = new ArrayList<>();
         ar2.addAll(arCopy);
         System.out.println("New ArrayList ar2 is : " + ar2);
-
     }
 
 }
-
